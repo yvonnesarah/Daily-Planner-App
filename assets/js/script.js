f71b41d06ab68de795abd7a56f2b8dd818118a28
Yvonne Adedeji <yvonne.sarah.adedeji@hotmail.com>
2025-02-05 19:45:16 +0000

    Added comments to these pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,54 @@
-// save button variable
+// Save button variable to target all save buttons on the page
 var saveBtn = $(".saveBtn");
 
-//current date shown on screen at the top
+// Display current date at the top of the screen 
 $("#currentDay").text(moment().format("dddd Do MMMM YYYY"));
 
-// color-coded to indicate whether it is in the past, present, or future on a certain time block
+// Function to color-code time blocks based on whether they are in the past, present, or future
 function ColorTimeBlock() {
-    var hour = moment().hours();
+    var hour = moment().hours(); // Get the current hour in 24-hour format
 
+    // Loop through each time block to compare the current time with the time block's hour
     $(".time-block").each(function() {
-        var currentHour = parseInt($(this).attr("id"));
+        var currentHour = parseInt($(this).attr("id")); // Get the hour of the current time block by its id
 
+        // Apply different CSS classes based on whether the time block is in the future, present, or past
         if (currentHour > hour) {
-            $(this).addClass("future");
+            $(this).addClass("future"); // Add 'future' class if the time block is in the future
         } else if (currentHour === hour) {
-            $(this).addClass("present");
+            $(this).addClass("present"); // Add 'present' class if the time block is the current time
         } else {
-            $(this).addClass("past");
+            $(this).addClass("past"); // Add 'past' class if the time block is in the past
         }
     })
 };
 
-// Click the save button for that time block to save
+// Event listener for when a save button is clicked
 saveBtn.on("click", function() {
 
+    // Get the time and description for the clicked time block
     var time = $(this).siblings(".hour").text();
     var description = $(this).siblings(".description").val();
 
-    // the text for each daily planner that is schedule at a time is saved using local storage
+    // Save the description for that specific time block in local storage
     localStorage.setItem(time, description);
 });
 
-// If you refresh the page the saved daily planner schedule persist
+// Function to load the saved daily schedule from local storage
 function DailyPlannerApp() {
 
+    // Loop through each hour block
     $(".hour").each(function() {
-        var currentHour = $(this).text();
-        var currentDescription = localStorage.getItem(currentHour);
+        var currentHour = $(this).text(); // Get the time of the current hour block
+        var currentDescription = localStorage.getItem(currentHour); // Retrieve the saved description from local storage
 
+        // If there is a saved description, populate the textarea for that time block
         if(currentDescription !== null) {
             $(this).siblings(".description").val(currentDescription);
         }
     });
 }
 
-// Call Functions to run the program of the daily planner app and to see the colour coded on the time blocks  
-
+// Call functions to apply color-coding to time blocks and load saved schedule
 ColorTimeBlock();
 DailyPlannerApp();
